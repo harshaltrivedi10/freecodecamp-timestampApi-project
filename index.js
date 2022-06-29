dc0837b501adf21f35433edd4a6c31d4cadb3309
Harshal Trivedi <harshal30897@gmail.com>
2022-06-28 23:09:43 -0700

    Added API endpoints and handled both the Date and UNIX Timestamp scenarios.

--- index.js
+++ index.js
@@ -6,27 +6,38 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/:date', function (req, res) {
+  if (/^\d+$/.test(req.params.date)) {
+    const unixTimestamp = parseInt(req.params.date);
+    const date = new Date(unixTimestamp).toUTCString();
+    res.json({
+      unix: unixTimestamp,
+      utc: date,
+    });
+  } else {
+    const unixTimestamp = Math.floor(new Date(req.params.date).getTime());
+    const date = new Date(req.params.date).toUTCString();
+    res.json({
+      unix: unixTimestamp,
+      utc: date,
+    });
+  }
 });
 
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+// listen for requests
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
